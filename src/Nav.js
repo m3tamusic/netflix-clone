243c31648761fc4483ad4545faffbc31f648d0aa
m3tamusic <sajanjacob67@gmail.com>
2020-09-24 04:22:18 -0600

    entire netflix build.

---
+++ src/Nav.js
@@ -0,0 +1,32 @@
+import React, {useState, useEffect} from "react";
+import "./Nav.css";
+function Nav() {
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
+        alt="Netflix Logo"
+      />
+
+      <img
+        className="nav__avatar"
+        src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/e70b1333850498.56ba69ac32ae3.png"
+        alt="Avatar"
+      />
+    </div>
+  );
+}
+
+export default Nav;
---
+++ src/Row.js
@@ -0,0 +1,72 @@
+import React, {useState, useEffect} from "react";
+import axios from "./axios";
+import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({title, fetchURL, isLargeRow}) {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchURL);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchURL]);
+
+  const opts = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+      color: "white"
+    }
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          // https://www.youtube.com/watch?v=XtMThy8QKqU&utm_source=email&ck_subscriber_id=841563010
+          // Grabbing the parameter '?v=XtMThy8QKqU&utm_source=email&ck_subscriber_id=841563010'👇
+          const urlParams = new URLSearchParams(new URL(url).search);
+          // Specifically grab the paramter after 'v'  so only 'XtMThy8QKqU' would be grabbed
+          // -> This is handy for links with multiple paramaters.
+          setTrailerUrl(urlParams.get("v"))
+          console.log(urlParams.get("v"));
+        })
+        .catch(error => console.log(error));
+    }
+  };
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {movies.map(movie => (
+          <img
+            key={movie.id}
+            onClick={() => handleClick(movie)}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+    </div>
+  );
+}
+
+export default Row;
